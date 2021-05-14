6ee6b466ef8d99296cfbbd153e28e106851076e5
DumaHycc <nuss_dmitriy@mail.ru>
2021-05-14 20:11:54 +0400

    Добавьте файлы проекта.

---
+++ Project1/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<ctime>
+using namespace std;
+
+void main()
+{
+	srand(time(NULL));
+
+	const int SIZE = 10;
+	int arr[SIZE];
+
+	bool already;
+
+	for (int i = 0; i < SIZE; )
+	{
+		already = false;
+		int newValue = rand() % 10;
+
+		for (int j = 0; j < i; j++) //провер€ет дубликат числа
+		{
+			if (arr[j] == newValue) 
+			{
+				already = true;
+				break;
+			}
+		}
+
+		if (!already)
+		{
+			arr[i] = newValue; //добавл€ем новый элемент
+			i++; //переходим на следующую €чейку
+		}
+	}
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << " | ";
+	}
+	cout << "\n\n";
+}
